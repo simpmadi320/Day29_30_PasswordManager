1af7b79fde4fcc18be8a59f4674738ab84fa7064
Madison Simpson <madison_simpson@outlook.com>
2024-02-28 19:25:32 -0500

    Added JSON

--- main.py
+++ main.py
@@ -2,10 +2,31 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 DEFAULT_EMAIL = "madison_simpson@outlook.com"
 
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+
+
+def get_password():
+    try:
+        with open("password.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="ERROR", message=f"The file could not be found")
+    else:
+        try:
+            username_e.delete(0, END)
+            username_e.insert(0, data[website_e.get()]["email"])
+            password_e.delete(0, END)
+            password_e.insert(0, data[website_e.get()]["password"])
+        except KeyError as error_message:
+            messagebox.showinfo(title="ERROR", message=f"The website {error_message} could not be found")
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
@@ -34,18 +55,35 @@ def add_password():
     user = username_e.get()
     password = password_e.get()
 
+    # For Json
+    new_data = {
+        web: {
+            "email": user,
+            "password": password
+        }
+    }
+
     if len(web) == 0 or len(user) == 0 or len(password) == 0:
         messagebox.showinfo(title="oops", message="Please make sure all fields are filled")
     else:
-        is_ok = messagebox.askokcancel(title=web, message=f"These are the details entered: \nEmail: {user}\n"
-                                                      f"Password: {password}\nIs it ok to save?")
-        if is_ok:
-            with open("passwords.txt", "a") as data_file:
-                data_file.write(f"{web} | {user} | {password}\r")
+        try:
+            with open("passwords.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+                #Updating old data with new data
+                data.update(new_data)
+        except FileNotFoundError:
+            data = new_data
+
+        with open("passwords.json", "w") as data_file:
+            # Saving updated data
+            json.dump(data, data_file, indent=4)
             website_e.delete(0, END)
             password_e.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
@@ -60,9 +98,12 @@ canvas.grid(column=1, row=0)
 website_l = Label(text="Website")
 website_l.grid(column=0, row=1)
 
-website_e = Entry(width=52)
+website_e = Entry(width=33)
 website_e.focus()
-website_e.grid(column=1, row=1, columnspan=2)
+website_e.grid(column=1, row=1)
+
+search_b = Button(text="Search", width=14, command=get_password)
+search_b.grid(column=2, row=1)
 
 # Row 3
 username_l = Label(text="Email/Username:")
